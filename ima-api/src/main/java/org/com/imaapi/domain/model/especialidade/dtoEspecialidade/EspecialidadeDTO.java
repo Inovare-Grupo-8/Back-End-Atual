8ad0d3df5dcf24c2f86ce5e1a80504cd92cd7a4b
giovannagomeslm <giovanna.macedo@sptech.school>
2025-09-05 14:10:52 -0300

    DTO especialidade

---
+++ ima-api/src/main/java/org/com/imaapi/domain/model/especialidade/dtoEspecialidade/EspecialidadeDTO.java
@@ -0,0 +1,30 @@
+package org.com.imaapi.domain.model.especialidade.dtoEspecialidade;
+
+
+public class EspecialidadeDTO {
+    private Integer id;
+    private String nome;
+
+    public EspecialidadeDTO() {}
+
+    public EspecialidadeDTO(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
