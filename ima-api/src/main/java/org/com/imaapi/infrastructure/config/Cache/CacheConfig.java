330617fddcfd39cca17d1f2f1b24c451c6472497
giovannagomeslm <giovanna.macedo@sptech.school>
2025-11-26 19:12:26 -0300

    update

--- ima-api/src/main/java/org/com/imaapi/infrastructure/config/Cache/CacheConfig.java
+++ ima-api/src/main/java/org/com/imaapi/infrastructure/config/Cache/CacheConfig.java
@@ -1,4 +1,4 @@
-package org.com.imaapi.infrastructure.config;
+package org.com.imaapi.infrastructure.config.Cache;
 
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
